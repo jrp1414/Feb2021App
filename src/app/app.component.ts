5a34480ad0141e8d37195ea6228752769bb67236
Jyotiranjan Panda <jyotiranjan.p14@gmail.com>
2021-02-19 09:24:08 +0530

    Component Examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  // selector: 'app-root',
+  // selector: '[app-root]',
+  selector: '.app-root',
+  // template: "<h1>Hello World. Practising Angular</h1>" +
+  //   "<div>Line 2</div>"
+  // template:`
+  // <h2>Multiple Line Template</h2>
+  // <h3>Using backtick to write multi line template</h3>
+  // `
+  templateUrl: "./app.component.html",
+  // styles: [
+  //   `
+  //   h2{
+  //     background-color:aqua;
+  //   }
+  //   `],
+  styleUrls: [
+    "./app.component.less"
+  ]
+  // encapsulation: ViewEncapsulation.ShadowDom
 })
 export class AppComponent {
-  title = 'Feb2020App2';
+  constructor() {
+
+  }
+}
+
+
+enum Color {
+  Red,
+  Green,
+  Blue
 }
