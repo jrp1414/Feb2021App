dd0d36d08120c168c243ea149e27c620c82583ed
Ranjan Panda <ranjan.net14@gmail.com>
2021-02-26 08:41:52 +0530

    Pipe examples completed

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -27,4 +27,19 @@ export class ProductsComponent {
     this.addedToCart.push(d);
   }
 
+  FilterTest(){
+    this.products.push({
+      "id":1,
+      "title": "Brown eggs",
+      "type": "dairy",
+      "description": "Raw organic brown eggs in a basket",
+      "filename": "0.jpg",
+      "height": 600,
+      "width": 400,
+      "price": 28.1,
+      "rating": 4,
+      "imageurl":"https://previews.123rf.com/images/bhofack2/bhofack21502/bhofack2150200615/37073865-raw-organic-brown-eggs-in-a-basket.jpg"
+    });
+  }
+
 }
--- src/app/shared/pipes/filter.pipe.ts
+++ src/app/shared/pipes/filter.pipe.ts
@@ -2,17 +2,28 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { Product } from 'src/app/services/product.model';
 
 @Pipe({
-  name: 'filter'
+  name: 'filter',
+  pure: false
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(list: Product[], searchVal: string): Product[] {
+  transform(list: any[], searchVal: string, ...filterColumns: string[]): Product[] {
     if (searchVal.length > 1) {
-      // return list.filter(function (val) {
-      //   return val.title.toLowerCase().indexOf(searchVal.toLowerCase()) !== -1;
-      // });
 
-      return list.filter((val) => val.title.toLowerCase().indexOf(searchVal.toLowerCase()) !== -1);
+      // return list.filter((val) => val.title.toLowerCase().indexOf(searchVal.toLowerCase()) !== -1);
+
+      let tempList: any[] = [];
+      for (let column of filterColumns) {
+        var filtered = list.filter((obj) => obj[column].toLowerCase().indexOf(searchVal.toLowerCase()) != -1);
+        // Array.prototype.push.apply(tempList, filtered);
+        tempList = [...tempList, ...filtered];
+
+        //[1,2,3]
+        //[2,3]
+        //[[1,2,3],[2,3]]
+        //[1,2,3,2,3]
+      }
+      return tempList;
     }
     return list;
   }
